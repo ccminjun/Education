ecde49252143492297840c0c30cdc4c85912da45
ccminjun <mi478@naver.com>
2021-05-04 12:46:58 +0900

    210504 오전 커밋

---
+++ OOPsolution/ClassTestApp/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassTestApp
+{
+    class Animal
+    {
+        private int UniqueId { get; set; }
+        protected string Name { get; set; } //Main에 오류가 남 protected라 설정을 안해주면 why 상속받은 자식한테만 사용 할 수 있기 때문에
+
+        public void Eat(string meal)
+        {
+            Console.WriteLine($"{this.Name} 이 {meal}을 먹습니다.");
+        }
+        public virtual void Sleep()
+        {
+            Console.WriteLine($"{this.Name}이(가) 잡니다/");
+        }
+    }
+}
---
+++ OOPsolution/ClassTestApp/Cat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTestApp
+{
+    class Cat : Animal
+    {
+       // public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+
+        public Cat() { /* Noting */ }  //시작 컴파일러가 만들어줌
+        public Cat(string name, string color, int age)
+        {
+            //부를 때 initalization (초기화) 라고 부름
+            this.Name = name;
+            this.Color = color;
+            this.Age = age;
+
+        }
+        
+        public Cat(string name, string color)
+        {
+            this.Name = name;
+            this.Color = color;
+        }
+
+        public Cat(string color, int age)  //칼라랑 네임 위치 바꾸면 컴퓨터 구분 못해서 오류가 남, string 두개 연달아 하면 안됨
+        {
+            this.Color = color;
+            this.Age = age;
+        }
+
+        public void Meow() { Console.WriteLine($"{this.Color} 고양이 {this.Name}, 야옹!"); } //책처럼 해도 괜찮지만 이게 더 편함.
+
+        public override void Sleep()  //override 를 통해 재정의하는 과정
+        {
+            // base.Sleep(); //부모가 한 일 다시 쓰는것 Animal에서 한 것 먼저 처리한 것 (부모의 Sleep()실행
+            // 이후 자식 클래스의 Sleep을 실행
+            Console.WriteLine($"{this.Color} 고양이 {this.Name}이(가) ZZ잡니다!");
+        }
+    }
+}
