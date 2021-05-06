ea63c37e3b04fc387a5bf3c401d0ecbe41049f3c
Hugo76-snd <personar95@gmail.com>
2021-05-06 09:48:59 +0900

    Basic C# CodingTest

---
+++ BasicCSharpCodingTest/Test04/MainApp4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Test04
+{
+    class MainApp4
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 7; i < 10; i++)
+            {
+                Console.WriteLine($"{i}단 계산 시작");
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.Write($"{i} * {j} = {i * j}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ BasicCSharpCodingTest/Test05/MainApp5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test05
+{
+    class MainApp5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3,6,9! 3,6,9! 3,6,9! 3,6,9!!");
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 15 == 0) Console.WriteLine();
+
+                if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9 ||
+                    i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+                    Console.Write("짝!\t");
+                else if (i == 0)
+                    Console.Write("짝!\t");
+                else 
+                    Console.Write($"{i}\t");                
+            }
+        }
+    }
+}
