dd28a090bd43245c53882d021cbeb795f5f01fa6
ccminjun <mi478@naver.com>
2021-05-10 17:49:10 +0900

    5월10일 끝

--- OOPsolution/EventHandlerCheckApp/.Designer.cs
+++ OOPsolution/EventHandlerCheckApp/.Designer.cs
@@ -1,7 +1,6 @@
-﻿
-namespace EventHandlerCheckApp
+﻿namespace EventHandlerCheckApp
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +28,80 @@ namespace EventHandlerCheckApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtUserID = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // TxtUserID
+            // 
+            this.TxtUserID.Location = new System.Drawing.Point(99, 24);
+            this.TxtUserID.Name = "TxtUserID";
+            this.TxtUserID.Size = new System.Drawing.Size(159, 23);
+            this.TxtUserID.TabIndex = 2;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(99, 61);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '●';
+            this.TxtPassword.Size = new System.Drawing.Size(159, 23);
+            this.TxtPassword.TabIndex = 3;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Location = new System.Drawing.Point(291, 50);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(77, 34);
+            this.BtnLogin.TabIndex = 4;
+            this.BtnLogin.Text = "로그인";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(403, 109);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUserID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "이벤트확인 앱";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtUserID;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button BtnLogin;
     }
 }
-
--- OOPsolution/EventHandlerCheckApp/MainForm.cs
+++ OOPsolution/EventHandlerCheckApp/MainForm.cs
@@ -10,85 +10,24 @@ using System.Windows.Forms;
 
 namespace EventHandlerCheckApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        private Label label1;
-        private Label label2;
-        private TextBox TxtuserID;
-        private TextBox TxtPassword;
-        private Button button1;
-
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
 
-        private void InitializeComponent()
+        private void BtnLogin_Click(object sender, EventArgs e)
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.TxtuserID = new System.Windows.Forms.TextBox();
-            this.TxtPassword = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "UserID";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 183);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Password";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // TxtuserID
-            // 
-            this.TxtuserID.Location = new System.Drawing.Point(142, 84);
-            this.TxtuserID.Name = "TxtuserID";
-            this.TxtuserID.Size = new System.Drawing.Size(302, 27);
-            this.TxtuserID.TabIndex = 2;
-            // 
-            // TxtPassword
-            // 
-            this.TxtPassword.Location = new System.Drawing.Point(142, 180);
-            this.TxtPassword.Name = "TxtPassword";
-            this.TxtPassword.Size = new System.Drawing.Size(302, 27);
-            this.TxtPassword.TabIndex = 3;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(490, 161);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 46);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "로그인";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.ClientSize = new System.Drawing.Size(656, 383);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.TxtPassword);
-            this.Controls.Add(this.TxtuserID);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            var result = TxtUserID.Text + "\n" + TxtPassword.Text;
+            MessageBox.Show($"당신의 아이디와 패스워드는 {result}");
         }
 
-        private void label2_Click(object sender, EventArgs e)
+
+
+
+
+        private void MainForm_Resize(object sender, EventArgs e)
         {
 
         }
